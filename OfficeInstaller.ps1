6a11688a6fd34e20e588ffc703cfab128b4c24e9
xl350r <danhsecure@gmail.com>
2022-09-17 11:03:07 -0700

    Initial

---
+++ OfficeInstaller.ps1
@@ -0,0 +1,42 @@
+﻿# OfficeInstaller.ps1
+
+function choice {
+    Switch (read-host ">") {
+        "32" {.\setup.exe setup.exe /configure ./32config.xml}
+        "64" {.\setup.exe /configure  ./64config.xml}
+        "Egg" {egg; choice;}
+        default {write-host "choose either 32 or 64!"; choice};
+    }
+}
+
+function egg () {
+write-host "
+   ###   
+  #   #  
+ #     # 
+ #     # 
+ #     # 
+  #   #  
+   ###   
+         
+"
+}
+
+function main {
+write-host "
+                                         ###                                                        
+  ####  ###### ###### #  ####  ######     #  #    #  ####  #####   ##   #      #      ###### #####  
+ #    # #      #      # #    # #          #  ##   # #        #    #  #  #      #      #      #    # 
+ #    # #####  #####  # #      #####      #  # #  #  ####    #   #    # #      #      #####  #    # 
+ #    # #      #      # #      #          #  #  # #      #   #   ###### #      #      #      #####  
+ #    # #      #      # #    # #          #  #   ## #    #   #   #    # #      #      #      #   #  
+  ####  #      #      #  ####  ######    ### #    #  ####    #   #    # ###### ###### ###### #    # 
+
+Author: Daniel Hoberecht
+Choose 32 or 64 Bit!!                                                                                                    
+"
+
+choice
+}
+
+main
